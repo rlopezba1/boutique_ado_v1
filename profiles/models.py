4758c19e6ab7a4b2841c944f6cb41e3bb7b44419
rlopezba1 <rubenlopezb@gmail.com>
2021-08-23 14:49:44 +0000

    Updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -23,6 +23,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
@@ -30,5 +31,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Exisiting users: just save the profile
-    instance.UserProfile.save()
+    # Existing users: just save the profile
+    instance.userprofile.save()
